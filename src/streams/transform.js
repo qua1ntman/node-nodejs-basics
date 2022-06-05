b852a5e8254aa954c854a25c03adf4522a2df71c
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 15:25:40 +0300

    feat:  implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout according to the task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import fs from 'fs'
+import path from 'path'
+import { stdin, stdout } from 'process';
+import { fileURLToPath } from 'url';
+
 export const transform = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    let str = ''
+    const writer = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'))
+    stdin.on('data', (data) => str+=data)
+
+    process.on('SIGINT', () => {
+        stdout.write(str.split("").reverse().join(""))
+        process.exit();
+    });
+    
 };
