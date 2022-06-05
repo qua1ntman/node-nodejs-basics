c1a3a293994405aace17fb356447d4c9e7c55503
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 12:57:29 +0300

    feat: implement function that creates new file according to task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,24 @@
+import fs from 'fs'
+import path from 'path'
+import {fileURLToPath} from 'url';
+
 export const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fresh.txt')
+    
+    fs.exists(filePath, (isExists) => {
+        if (isExists) throw Error('FS operation failed')
+    })
+
+    fs.writeFile(filePath, '', (err) => {
+        if (err) throw err
+    });
+
+    fs.appendFile(filePath, 
+        'I am fresh and young',
+        (err) => {
+          if (err) throw err.message
+        }
+    );
 };
