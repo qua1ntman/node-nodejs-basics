3571afdd41627aa196aa4e34243bba2e830db1e3
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 13:39:33 +0300

    feat: implement function that parses command line arguments according to the task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,5 @@
 export const parseArgs = () => {
-    // Write your code here 
+    process.argv.forEach(function (val, index, array) {
+        console.log(`--prop${index === 0 ? '' : index+1}Name ${val}`);
+    });
 };
