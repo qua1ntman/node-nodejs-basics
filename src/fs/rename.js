267519dff2fa327563d74a5b585ab96718f775f6
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 13:31:49 +0300

    feat: implement function that renames file according to the task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 export const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.exists(path.join(__dirname, 'files', 'wrongFilename.txt'), (isExists) => {
+        if (!isExists) throw Error('FS operation failed')
+    }) 
+
+    fs.exists(path.join(__dirname, 'files', 'properFilename.md'), (isExists) => {
+        if (isExists) throw Error('FS operation failed')
+    })
+
+    fs.rename(path.join(__dirname, 'files', 'wrongFilename.txt'), path.join(__dirname, 'files', 'properFilename.md'), () => {});
+
 };
