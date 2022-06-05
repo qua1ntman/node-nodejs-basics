1b34b1f583c4a25e8302ac42ccfa5b49ec06d634
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 13:33:36 +0300

    feat: implement function that prints all array of filenames from files folder into console according to the task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,29 @@
+import fs, {stat} from 'fs'
+import {readdir} from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.exists(path.join(__dirname, 'files'), (isExists) => {
+        if (!isExists) throw Error('FS operation failed')
+    })
+
+    readdir(path.join(__dirname, 'files'), {
+        withFileTypes: true
+    }).then(response => {
+        response.forEach((stats) => {
+            const filepath = path.join(__dirname, 'files', stats.name);
+            stat(filepath, (error, st) => {
+                if (error) return console.log(error.message);
+                if (st.isFile()) {
+                    console.log(stats.name);
+                }
+            });
+        });
+    });
 };
+
+list()
