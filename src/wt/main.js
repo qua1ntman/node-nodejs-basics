12ee535645244cf3b457755d99d40cb8bb086ca2
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 15:35:39 +0300

    feat: implement function that creates number of worker threads from file and able to send data to those threads and to receive result of the computation from them according to the task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,28 @@
+import { Worker } from 'worker_threads';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import os from 'os'
+
 export const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename)
+    const coreNumber = os.cpus().length
+    const resultArr = []
+    let baseNum = 10
+
+    for (let i = 0; i < coreNumber; i++) {  
+        const worker = new Worker(path.join(__dirname, 'worker.js'), {workerData: baseNum+i})
+        const resultObj = {status: 'resolved', data: ''}
+        worker.on('error', (e) => resultObj.status = 'error')
+        worker.on('message', (result) => {
+            resultObj.data = result
+            resultArr.push(resultObj)
+        })
+        worker.on('exit', () => {
+            if (resultArr.length == coreNumber) {
+                resultArr.sort((a, b) => a.data - b.data)
+                console.log(resultArr);
+            }      
+        })
+    }  
 };
