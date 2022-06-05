cc71aea751c789213c56172bd3ea47f8adc8ad2b
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 13:34:14 +0300

    feat: implement function that prints content of the file according to the task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
 export const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    fs.readFile(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf8', function(err, data){
+        stdout.write(data)
+    });
 };
+
