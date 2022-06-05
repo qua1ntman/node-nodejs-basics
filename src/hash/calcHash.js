f030e6f26ac4f868b768e6c5c5a72a10c0f2d563
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 15:16:29 +0300

    feat: implement function that calculates SHA256 hash according to the task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import crypto from "crypto";
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const fileBuffer = fs.readFileSync(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    const hashSum = crypto.createHash('sha256');
+
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
 };
