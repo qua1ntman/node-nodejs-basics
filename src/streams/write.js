a22a6d511a85d03fae4e5e748077789c17c1e025
Mikhail Ashla <redikfrost@gmail.com>
2022-06-05 15:23:56 +0300

    feat: implement function that writes process.stdin data into content using Writable Stream according to the task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,12 @@
+import fs from 'fs'
+import path from 'path'
+import { stdin } from 'process';
+import { fileURLToPath } from 'url';
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const writer = fs.createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'))
+    stdin.on('data', (data) => writer.write(data))
 };
